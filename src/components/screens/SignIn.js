f0ea116b237883f8cd0f31191e56e99b97897a73
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-29 10:30:10 +0700

    Bagus [Dev] Create view for SignIn

---
+++ src/components/screens/SignIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SignIn = () => {
+    return(
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="password"></input>
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    Login
+                </button>
+                <h5>
+                    <Link to="/signup">Dont have an account ?</Link>
+                </h5>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn;
