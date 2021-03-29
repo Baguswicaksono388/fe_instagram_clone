903ef699064bbe98f7ea4c39ae6b24d951f54d1f
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-29 10:29:45 +0700

    Bagus [Dev] Mofikasi syntax di file Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return(
         <nav>
             <div className="nav-wrapper white">
-            <a href="#" className="brand-logo left">Logo</a>
+            <Link to="/" className="brand-logo left">Instagram</Link>
             <ul id="nav-mobile" className="right">
-                <li><a href="/login">Login</a></li>
-                <li><a href="/signup">Signup</a></li>
-                <li><a href="/profile">Profile</a></li>
+                <li><Link to="/signin">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
             </ul>
             </div>
         </nav>
