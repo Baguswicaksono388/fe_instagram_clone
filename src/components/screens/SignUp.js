047654081951c47eed39d017ed06c323bb43337a
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-29 10:27:00 +0700

    Bagus [Dev] create SignUp on components/screens

--- src/components/screens/SignUp.js
+++ src/components/screens/SignUp.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const SignUp = () => {
     return(
-        <h1>SignUp</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="Name"></input>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="Password"></input>
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+                    SignUP
+                </button>
+                <h5>
+                    <Link to="/signin">Already have an account ?</Link>
+                </h5>
+            </div>
+        </div>
     )
 }
 
