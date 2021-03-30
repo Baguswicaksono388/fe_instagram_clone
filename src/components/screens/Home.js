168adad82e332e08ead0667c967af74d5c3b4b8a
Bagus Wicaksono <baguswicaksono388@gmail.com>
2021-03-30 10:35:38 +0700

    Bagus [Dev] UI Home

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>bagus</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>bagus</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"/>
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>title</h6>
+                    <p>this is amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
