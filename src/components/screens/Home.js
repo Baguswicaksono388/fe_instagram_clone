49ae2056db2af70e5fdf07ba979b81b270522ac9
Bagus <sgiapi1@gmail.com>
2021-05-07 16:29:52 +0700

    Bagus [Dev] Implemensts API like & unlike

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -10,10 +10,43 @@ const Home = () => {
             }
         }).then(res=>res.json())
         .then(result=> {
-            // console.log(result);
+            console.log(result);
             setData(result.data);
         })
     },[])
+
+    const likePosts = (id) => {
+        fetch('/like', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+            console.log(result);
+        })
+    }
+
+    const unlikePosts = (id) => {
+        fetch('/unlike', {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer "+localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+            console.log(result);
+        })
+    }
+
     return (
         <div className="home">
             {
@@ -26,6 +59,9 @@ const Home = () => {
                             </div>
                             <div className="card-content">
                                 <i className="material-icons" style={{color:"red"}}>favorite</i>
+                                <i className="material-icons" onClick={()=>{likePosts(item._id)}}>thumb_up</i>
+                                <i className="material-icons" onClick={()=>{unlikePosts(item._id)}} >thumb_down</i>
+                                <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
                                 <input type="text" placeholder="add a comment"/>
